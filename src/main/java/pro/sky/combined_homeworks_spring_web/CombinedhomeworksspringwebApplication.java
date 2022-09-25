458c27f01596a4f5f067abc88a9dfc31fe6a752d
Denis <demaz@bk.ru>
2022-09-25 19:50:19 +0700

    Initial commit

---
+++ src/main/java/pro/sky/combined_homeworks_spring_web/CombinedhomeworksspringwebApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.combined_homeworks_spring_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CombinedhomeworksspringwebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CombinedhomeworksspringwebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/combined_homeworks_spring_web/CombinedhomeworksspringwebApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.combined_homeworks_spring_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CombinedhomeworksspringwebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
